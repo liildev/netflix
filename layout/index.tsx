151bdbb0be71a5aefac18cc542d3738e3cee7754
mansourov <mansurov.jr@mail.ru>
2022-11-05 14:56:09 +0500

    meta added

--- layout/index.tsx
+++ layout/index.tsx
@@ -10,9 +10,13 @@ export default function MainLayout({ title, children }: Props) {
     return (
         <>
             <Head>
-                <title>{title ? `${title} - Netflix` : 'Netflix'}</title>
+                <meta name="robots" content="index, follow" />
+                <meta name="keywords" content="next, javascript, react" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta charSet="utf-8" />
                 <meta name="google-site-verification" content="6qiEMoDqlbT475KWutakgNWa-eapCgPIbQFd3n_VTDM" />
                 <link rel="icon" href="/netflix.webp" />
+                <title>{title ? `${title} - Netflix` : 'Netflix'}</title>
             </Head>
             {
                 children
